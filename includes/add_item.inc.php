49effcc514f053eee2050f1a264ddfe660755067
RCMGuy <64056589+RCMGuy@users.noreply.github.com>
2020-08-23 14:42:11 +1000

    Update add_item.inc.php

--- includes/add_item.inc.php
+++ includes/add_item.inc.php
@@ -1,10 +1,10 @@
 <?php
 
 //include connect to database
-include_once 'dbh.inc.php';
+include_once 'dbh.inc.php'; //great time-saver technique for inserting values in database - Riley
 
 //get the form inputs
-$inputDate = $_POST['date'];
+$inputDate = $_POST['date']; //great naming convetion for user to identify what input is what. - Riley
 $inputName = $_POST['name'];
 $inputWeight = $_POST['weight'];
 $inputCost = $_POST['cost'];
